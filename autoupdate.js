39f9246c2b6e64aa4ba1ab1fcb5f6fa867b7bcf4
DrewJSquared <127908019+DrewJSquared@users.noreply.github.com>
2023-06-30 09:54:47 -0500

    initial

---
+++ autoupdate.js
@@ -0,0 +1,63 @@
+// autoupdate.js
+// automatic updater script for Attitude Control software
+// copyright 2023 Drew Shipps, J Squared Systems
+
+
+// import log
+var log = require('npmlog');
+
+log.info('AUTOUPDATE', 'Automatic Update Script for Attitude Control Device Firmware');
+log.info('AUTOUPDATE', 'Copyright 2023 Drew Shipps, J Squared Systems');
+
+
+var fs = require('fs')
+
+
+// remove old backup
+fs.exists('backup/AttitudeControl', function(exists) {
+	if (exists) {
+		log.info('AUTOUPDATE', 'Backup code found, deleting now...');
+		fs.rmSync('backup/AttitudeControl', { recursive: true, force: true });
+		log.info('AUTOUPDATE', 'Deleted backup code.');
+	} else {
+		log.info('AUTOUPDATE', 'No backup code found.');
+	}
+});
+
+
+// move current to backup
+setTimeout(function () {
+	fs.exists('AttitudeControl', function(exists) {
+		if (exists) {
+			log.info('AUTOUPDATE', 'Old code found, moving to backup folder...');
+			fs.rename('AttitudeControl', 'backup/AttitudeControl', function (err) {
+				if (err) {
+					log.error('AUTOUPDATE', err.message);
+				} else {
+					log.info('AUTOUPDATE', 'Moved old code to backup folder.');
+				}
+			});
+		} else {
+			log.info('AUTOUPDATE', 'No old code found.');
+		}
+	});
+}, 1000);
+
+
+// download repo
+const download = require('download-git-repo');
+
+setTimeout(function () {
+	log.info('AUTOUPDATE', 'Downloading new code...');
+	download('drewjsquared/attitudecontrol', './AttitudeControl', function (err) {
+		if (err) {
+			log.error('AUTOUPDATE', err.message);
+		} else {
+			log.info('AUTOUPDATE', 'Download code success!');
+
+			setTimeout(function () {
+				log.info('AUTOUPDATE', 'Rebooting device now...');
+			}, 2000);
+		}
+	})
+}, 2000);
